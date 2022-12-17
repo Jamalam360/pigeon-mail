b76e44ef6c7e6449ccfd91583efac48e417ff893
James (Jamalam) <james@jamalam.tech>
2022-12-17 20:45:27 +0000

    feat: seo stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,18 @@
 import { defineConfig } from "astro/config";
-
 import preact from "@astrojs/preact";
 import cloudflare from "@astrojs/cloudflare";
 import tailwind from "@astrojs/tailwind";
+import robotsTxt from "astro-robots-txt";
+
+import compress from "astro-compress";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), tailwind()],
+  integrations: [preact(), tailwind(), compress(), sitemap(), robotsTxt()],
   output: "server",
-  adapter: cloudflare({ mode: "directory" }),
+  adapter: cloudflare({
+    mode: "directory",
+  }),
 });
